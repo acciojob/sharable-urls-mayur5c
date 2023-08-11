c3aaaf714dc23873fb435bd4def18c26679fce2e
Mayur Chopra <mayur5c@users.noreply.github.com>
2023-08-11 11:58:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // your code here
+function updateURL() {
+  var name = document.getElementById("name").value;
+  var year = document.getElementById("year").value;
+  var url = document.getElementById("url").innerHTML;
+  url += "?name=" + name;
+  url += "&year=" + year;
+  document.getElementById("url").innerHTML = url;
+}
