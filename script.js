ed01697f0af753b28d5bf5ad410cfe60e1aea46a
Mayur Chopra <mayur5c@users.noreply.github.com>
2023-10-09 00:16:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-// your code here
-function updateURL() {
-  var name = document.getElementById("name").value;
-  var year = document.getElementById("year").value;
-  var url = document.getElementById("url").innerHTML;
-  url += "?name=" + name;
-  url += "&year=" + year;
-  document.getElementById("url").innerHTML = url;
+var form = document.querySelector("form");
+var h3 = document.querySelector("h3");
+var nameElement = document.querySelector("#name");
+var yearElement = document.querySelector("#year");
+var btn = document.querySelector("#button");
+ 
+function sharableURL(event) {
+	event.preventDefault();
+	var h3value = "https://localhost:8080/"
+	var name = nameElement.value;
+	var year = yearElement.value;
+	
+	if(name && year){
+		h3value += "?name=" + name + "&year=" + year;
+	}
+	else if(name && !year){
+		h3value += "?name=" + name;
+	}
+	else if(!name && year){
+		h3value += "?year=" + year;
+	}
+	h3.textContent = h3value;
 }
+form.addEventListener("submit",sharableURL);
